d19f075d1b8ae36a30e96ea18892764c141f3851
ccgjosh <ccgjosh@gmail.com>
2016-03-07 10:38:02 -0500

    Added Taxonomy
    Allow the taxonomy set with a module.

--- class-zestsms-builder.php
+++ class-zestsms-builder.php
@@ -9,12 +9,21 @@ final class ZestSMSBuilder {
 		global $post;
 
 		$modules = FLBuilderModel::$modules;
-		$all_field_meta = self::find_meta_fields($modules, 'defaults');
+		$all_fields = self::find_fields($modules, 'defaults');
 
-		if(array_key_exists($form_type, $all_field_meta)) {
-			foreach($all_field_meta[$form_type] as $field => $key) {
-				if($field_meta = get_post_meta($post->ID, $key, true)) {
-					$defaults->$field = $field_meta;
+		if(is_array($all_fields)) {
+			foreach($all_fields as $parent_form => $types) {
+				foreach($types as $type => $fields) {
+					foreach($fields as $field => $default) {
+						if($parent_form == $form_type) {
+							if($type == 'meta') {
+								$defaults->$field = get_post_meta($post->ID, $default['key'], true);
+							}
+							if($type == 'taxonomy') {
+								$defaults->$field = wp_get_post_terms($post->ID, $default['key'], array("fields" => "ids"));
+							}
+						}
+					}
 				}
 			}
 		}
@@ -22,8 +31,8 @@ final class ZestSMSBuilder {
 		return $defaults;
 	}
 
-	static public function find_meta_fields( $modules = null, $return = 'meta' ) {
-		$meta = array();
+	static public function find_fields( $modules = null, $return = null ) {
+		$fields = array();
 
 		if(!is_null($modules)) {
 			foreach($modules as $module) {
@@ -32,17 +41,28 @@ final class ZestSMSBuilder {
 					foreach($tab_args['sections'] as $section => $section_args) {
 						if(!is_array($section_args['fields'])) continue;
 						foreach($section_args['fields'] as $field => $field_args) {
-							if($field_args['meta']) {
-								$key = ($field_args['meta']['key']) ? $field_args['meta']['key'] : $field;
-								$val = ($field_args['meta']['value']) ? $field_args['meta']['value'] : $module->settings->$field;
-								if($return == 'meta') {
-									$meta[$module->node][$field] = array(
-										'key'	=> $key,
-										'val'	=> $val
-									);
-								} else if($return == 'defaults') {
-									$meta[$module->slug . '-module'][$field] = $key;
+							if($field_args['meta'] || $field_args['taxonomy']) {
+								if($field_args['meta']) {
+									$type = 'meta';
+									$key = ($field_args['meta']['key']) ? $field_args['meta']['key'] : $field;
+									$val = ($field_args['meta']['value']) ? $field_args['meta']['value'] : $module->settings->$field;
 								}
+								if($field_args['taxonomy']) {
+									$type = 'taxonomy';
+									$key = $field_args['taxonomy'];
+									$val = $module->settings->$field;
+								}
+
+								// Update the database
+								$fields[$module->node][$type][$field] = array(
+									'key'	=> $key,
+									'val'	=> $val
+								);
+								// Set defaults
+								$fields['defaults'][$module->slug . '-module'][$type][$field] = array(
+									'key' => $key,
+									'multiple' => ($field_args['multiple']) ? false : true
+								);
 							}
 						}
 					}
@@ -50,25 +70,41 @@ final class ZestSMSBuilder {
 			}
 		}
 
-		return $meta;
+		if($return == 'defaults') {
+			return $fields['defaults'];
+		} else {
+			unset($fields['defaults']);
+			return $fields;
+		}
 	}
 
 	static public function save_fl_builder_data( $meta_id, $post_id, $meta_key, $meta_value ){
 
 		if('_fl_builder_data' === $meta_key) {
 			$modules = FLBuilderModel::get_all_modules();
-			$meta = self::find_meta_fields($modules, 'meta');
+			$nodes = self::find_fields($modules);
 
-			if(!empty($meta)) {
+			if(!empty($nodes)) {
 				$draft = maybe_unserialize(get_post_meta($post_id, '_fl_builder_draft', true));
 				$meta_value = maybe_unserialize( $meta_value );
-				foreach($meta as $node_id => $fields) {
-					foreach($fields as $field => $args) {
-						update_post_meta($post_id, $args['key'], $args['val']);
-						unset($meta_value[$node_id]->settings->$field);
-						unset($draft[$node_id]->settings->$field);
+				foreach($nodes as $node_id => $types) {
+				 	foreach($types as $type => $fields) {
+						foreach($fields as $field => $args) {
+							if('fl-builder-template' !== get_post_type($post_id)) {
+								if($type == 'meta') {
+									update_post_meta($post_id, $args['key'], $args['val']);
+								}
+								if($type == 'taxonomy') {
+									wp_set_post_terms($post_id, $args['val'], $args['key']);
+								}
+							}
+
+							if($meta_value[$node_id]->settings->$field) unset($meta_value[$node_id]->settings->$field);
+							if($draft[$node_id]->settings->$field) unset($draft[$node_id]->settings->$field);
+						}
 					}
 				}
+
 				update_post_meta( $post_id, $meta_key, $meta_value );
 				update_post_meta( $post_id, '_fl_builder_draft', $draft );
 			}
